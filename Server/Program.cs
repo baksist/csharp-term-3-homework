51d67ea263835022f19680cfd82762673857d11d
matt <matvey.serdukov@gmail.com>
2021-01-03 19:19:50 +0300

    custom URL support

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,19 +2,27 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Server.Models;
+using System.Text.RegularExpressions;
+using Server.Utils;
 
 namespace Server
 {
-    // TODO: add IP and port settings
     public class Program
     {
         public static List<Message> Messages = new List<Message>();
         public static List<ChatUser> Users = new List<ChatUser>();
         public static List<ActiveUser> ActiveUsers = new List<ActiveUser>();
-        
+        private static string customURL = "http://localhost:5000";
         
         public static void Main(string[] args)
         {
+            
+            if (args.Length == 2 && RegexUtils.MatchParams(args[0], args[1]))
+            {
+                var IP = args[0];
+                var port = args[1];
+                customURL = $"http://{IP}:{port}";
+            }
             Logger.LoadLog();
             Logger.LoadUsers();
             CreateHostBuilder(args).Build().Run();
@@ -22,6 +30,10 @@ namespace Server
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls(customURL);
+                });
     }
 }
---
+++ Server/Utils/RegexUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Server.Utils
+{
+    public class RegexUtils
+    {
+        const string validIP = @"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+        const string validHostname = @"^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$";
+        const string validPort = @"^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$";
+
+        public static bool MatchParams(string ip, string port)
+        {
+            return (Regex.IsMatch(ip, validIP) || Regex.IsMatch(ip, validHostname)) && 
+                   Regex.IsMatch(port, validPort);
+        }
+    }
+}
